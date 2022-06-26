061e5414f1365fe138ff8d948dc3e1c056bd7c39
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-26 18:15:51 -0300

    :construction: get state and city by IBGE API

---
+++ assets/scripts/register.js
@@ -0,0 +1,32 @@
+const stateSelectElement = document.querySelector("#state");
+const citySelectElement = document.querySelector("#city");
+
+const getBrazilianStates = async () => {
+  const response = await fetch(
+    "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+  );
+  const brazilianStates = await response.json();
+
+  brazilianStates.forEach((state) => {
+    stateSelectElement.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+  });
+};
+
+getBrazilianStates();
+
+stateSelectElement.addEventListener("change", getCitiesByStateId);
+
+async function getCitiesByStateId(event) {
+  const selectedStateId = event.target.value;
+
+  const response = await fetch(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedStateId}/municipios`
+  );
+
+  const getCities = await response.json();
+
+  getCities.forEach((city) => {
+    citySelectElement.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+  });
+  citySelectElement.removeAttribute("disabled");
+}
