3781d0cd551a81f29237dbe1e96e3bd035bb535c
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-06-27 21:56:07 -0300

    :construction: Layout and functionality to select collection items

---
+++ assets/scripts/register_selectedItems.js
@@ -0,0 +1,37 @@
+// Select items
+
+const colectItemsElements = document.querySelectorAll(".collectItems__item");
+const registerFormElement = document.querySelector("[data-js=register__form]");
+
+const markColectItemLiAsSelected = (event) =>
+  event.target.classList.toggle("collectItems__item--selected");
+
+const getSelectedCollectionItems = () => {
+  const selectedsColectItemsElements = document.querySelectorAll(
+    ".collectItems__item--selected "
+  );
+
+  let getSelectItemsValues = [];
+  selectedsColectItemsElements.forEach((item) => {
+    getSelectItemsValues = [
+      ...getSelectItemsValues,
+      item.dataset.collectitemid,
+      // itemName: item.dataset.collectitemname,
+    ];
+  });
+
+  const hidenInputToSendValues = document.querySelector(
+    "[data-js=collectItemsSelected]"
+  );
+  hidenInputToSendValues.value = getSelectItemsValues;
+  return getSelectItemsValues;
+};
+
+colectItemsElements.forEach((colectItemsLi) => {
+  colectItemsLi.addEventListener("click", markColectItemLiAsSelected);
+});
+
+registerFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getSelectedCollectionItems();
+});
